8baab49797e12c980f8c4ebed71bed119d18aa23
FilterBotOwner <zeba2862@gmail.com>
2025-01-03 18:02:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://crooked-urial-moviehorn1-ecdfe1d9.koyeb.app/")
+URL = environ.get("URL", "https://crooked-kimmi-moviehorn-8a06e72c.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
